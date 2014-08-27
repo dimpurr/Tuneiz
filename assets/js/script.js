e0b01ad7f5d8c31961528e06094af03249c07fca
dimpurr <dimpurr@live.com>
2014-08-27 11:06:09 +0800

    1.0 统计 Fin

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ nav.onclick = function scrollTop() {
 function sendTj() {
 	var xmlhttp;
 	if (window.XMLHttpRequest) { xmlhttp=new XMLHttpRequest(); } else { xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); };
-	xmlhttp.open("GET","http://work.dimpurr.com/theme/theme_tj.php?theme_name=Tuneiz&blog_url=Test&t=" + Math.random(),true);
+	xmlhttp.open("GET","http://work.dimpurr.com/theme/theme_tj.php?theme_name=Tuneiz&blog_url=" + window.location.host + "&t=" + Math.random(),true);
 	xmlhttp.send();
 }
 
