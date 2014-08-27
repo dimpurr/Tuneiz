909e12699fd7d8b44aa084152deb51542da0f719
dimpurr <dimpurr@live.com>
2014-08-27 12:46:27 +0800

    1.0.1 统计修复、滚动动画和 jQuery 化

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,20 +23,29 @@ function scroll_nav() {
 }
 
 var nav = document.getElementById("nav");
+var logo = document.getElementById("logo");
 var vh = window.screen.height;
 var vhh = vh * 0.5;
 
 window.onscroll = scroll_nav;
 
 nav.onclick = function scrollTop() {
-	document.body.scrollTop = document.documentElement.scrollTop = 0;
+	// document.body.scrollTop = document.documentElement.scrollTop = 0;
+	$(document.body).animate({scrollTop:$("#header").offset().top}, 1000 );
 };
 
+logo.onclick = function scrollCon() {
+	 $(document.body).animate({scrollTop:$("#content").offset().top}, 400 );
+}
+
 function sendTj() {
-	var xmlhttp;
-	if (window.XMLHttpRequest) { xmlhttp=new XMLHttpRequest(); } else { xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); };
-	xmlhttp.open("GET","http://work.dimpurr.com/theme/theme_tj.php?theme_name=Tuneiz&blog_url=" + window.location.host + "&t=" + Math.random(),true);
-	xmlhttp.send();
+//	var xmlhttp;
+//	if (window.XMLHttpRequest) { xmlhttp=new XMLHttpRequest(); } else { xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); };
+//	xmlhttp.open("GET","http://work.dimpurr.com/theme/theme_tj.php?theme_name=Tuneiz&blog_url=" + window.location.host + "&t=" + Math.random(),true);
+//	xmlhttp.send();
+
+	$.get("http://work.dimpurr.com/theme/theme_tj.php?theme_name=Tuneiz&blog_url=" + window.location.host + "&t=" + Math.random());
+
 }
 
 sendTj();
